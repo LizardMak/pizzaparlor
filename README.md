a55f38003c50bcdf23442cfa41ac2641a4e0fb55
Hunter Empey <LizardMak@gmail.com>
2023-11-03 10:35:36 -0600

    log pizza values

---
+++ README.md
@@ -0,0 +1,8 @@
+Describe: handleOrder()
+
+Test: "It should keep track of what size and toppings were chosen"
+Code: pizzaSize = fourteenInch 
+      checkedBoxes = pepperoni, greenPepper
+Expected Output: 14
+                ['greenPepper', 'pepperoni']
+                  
---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function PizzaOrder(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+window.addEventListener("load", function() {
+  this.document.getElementById("pizzaOrder").addEventListener("submit", handleOrder)
+})
+
+function handleOrder() {
+  event.preventDefault();
+  const pizzaSize = document.querySelector("input[name=size]:checked").value;
+  let pizzaToppings = [];
+  let checkedBoxes = document.querySelectorAll("input[name=toppings]:checked")
+  for (i = 0; i < checkedBoxes.length; i++) {
+    pizzaToppings.push(checkedBoxes[i].value)
+  }
+  console.log(pizzaSize);
+  console.log(pizzaToppings);
+}
