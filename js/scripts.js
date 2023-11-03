049a5ab23d5289fa8df590517cb6b91f32702fd4
Hunter Empey <LizardMak@gmail.com>
2023-11-03 12:44:11 -0600

    add total screen

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ function handleOrder() {
   }
   let myOrder = new PizzaOrder(pizzaSize, pizzaToppings)
   let total = myOrder.calculateTotal();
-  console.log(total); 
+  displayTotal(total); 
 }
 
 PizzaOrder.prototype.calculateTotal = function() {
@@ -25,4 +25,10 @@ PizzaOrder.prototype.calculateTotal = function() {
     price += 1;
   }
   return price
+}
+
+function displayTotal(total) {
+  document.getElementById("menu").setAttribute("class", "hidden");
+  document.getElementById("insertTotal").append(total);
+  document.getElementById("totalPage").setAttribute("class", "total")
 }
